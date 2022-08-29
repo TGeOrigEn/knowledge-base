e5acdf758301a5858469ad8ed431461e9f55dbd6
Тисло Артём Максимович <tgeorigen@mail.ru>
2022-08-29 07:24:34 +0600

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Header :text="'Фамилия'"></Header>
+</template>
+
+<script>
+import Button from '@/components/Button';
+import Header from '@/components/Header';
+
+export default {
+  components: {
+    Button,
+    Header
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    log() {
+      console.log("123");
+    }
+  }
+}
+</script>
+
+<style>
+</style>
