11ae5802691e0682c34d207e5f55644b96898b9d
Тисло Артём Максимович <tgeorigen@mail.ru>
2022-09-07 16:50:00 +0600

    1245125

--- src/controllers/PersonController.js
+++ src/controllers/PersonController.js
@@ -3,7 +3,7 @@ const database = require('../entities/DataBase');
 class PersonController {
     async createPerson(req, res) {
         console.log(req.body);
-        const {rank, career, activity, fullName, education, property, awards, salary, family } = req.body;
+        const { rank, career, activity, fullName, education, property, awards, salary, family } = req.body;
 
         const newPerson = await database.query('INSERT INTO person (property, awards, salary, family) values ($1, $2, $3, $4) RETURNING *;', [property, awards, salary, family]);
 
@@ -15,21 +15,21 @@ class PersonController {
 
         activity.forEach(async element => {
             await database.query('INSERT INTO activity (person_id, activity, description, place) ' +
-            'values ($1, $2, $3, $4) RETURNING *;', [newPerson.rows[0].id, element.activity, element.description, element.place]);
+                'values ($1, $2, $3, $4) RETURNING *;', [newPerson.rows[0].id, element.activity, element.description, element.place]);
         });
 
         career.forEach(async element => {
             await database.query('INSERT INTO career (person_id, startDate, endDate, career, place) ' +
-            'values ($1, $2, $3, $4, $5) RETURNING *;', [newPerson.rows[0].id, element.startDate, element.endDate, element.career, element.place]);
+                'values ($1, $2, $3, $4, $5) RETURNING *;', [newPerson.rows[0].id, element.startDate, element.endDate, element.career, element.place]);
         });
 
         rank.forEach(async element => {
             await database.query('INSERT INTO rank (person_id, startDate, endDate, degree, rank) ' +
-            'values ($1, $2, $3, $4, $5) RETURNING *;', [newPerson.rows[0].id, element.startDate, element.endDate, element.degree, element.rank]);
+                'values ($1, $2, $3, $4, $5) RETURNING *;', [newPerson.rows[0].id, element.startDate, element.endDate, element.degree, element.rank]);
         });
 
-        await database.query('UPDATE person SET education_id = ($2) WHERE id = ($1);', [ newPerson.rows[0].id, newEducation.rows[0].id ]);
-        await database.query('UPDATE person SET full_name_id = ($2) WHERE id = ($1);', [ newPerson.rows[0].id, newFullName.rows[0].id ]);
+        await database.query('UPDATE person SET education_id = ($2) WHERE id = ($1);', [newPerson.rows[0].id, newEducation.rows[0].id]);
+        await database.query('UPDATE person SET full_name_id = ($2) WHERE id = ($1);', [newPerson.rows[0].id, newFullName.rows[0].id]);
 
         res.json(newPerson.rows[0]);
     }
@@ -40,8 +40,9 @@ class PersonController {
     }
 
     async getPerson(req, res) {
-        const persons = await database.query('SELECT * FROM person WHERE id = $1;' [req.params.id]);
-        res.json(persons.rows[0]);
+        const id = req.params.id;
+        const person = await database.query('SELECT * FROM person WHERE id = $1;'[id]);
+        res.json(person.rows[0]);
     }
 
     async updatePerson(req, res) {
