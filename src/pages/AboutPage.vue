0f946c88418d9f390bde603bb60db74de8cafde8
Тисло Артём Максимович <tgeorigen@mail.ru>
2022-10-01 00:03:02 +0600

    1235124512

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div style="padding: 25px; display: flex; align-items: center;">
+        <h1 style="font-size: 50px; flex: 5;">Управленческая элита Западно-сибирского и степного генерал-губернаторств
+        </h1>
+        <input type="button" onclick="location.href='#/table';" style="flex: 1; font-size: 20px; height: 50px;" value="БАЗА ЗНАНИЙ"/>
+    </div>
+    <h2 style="padding: 25px; margin: 0; padding-bottom: 0; font-size: 38px;">Описание:</h2>
+    <h3 style="padding: 25px; padding-top: 0;">Описания пока нет, но оно точно будет !!!</h3>
+</template>
+  
+<script>
+export default {
+}
+</script>
+  
+<style>
+
+</style>
+  
--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import AdminPage from './pages/AdminPage.vue'
 import TablePage from './pages/TablePage.vue'
+import AboutPage from './pages/AboutPage.vue'
 
 export default createRouter({
     history: createWebHashHistory(),
     routes: [
         { path: '/admin', component: AdminPage },
-        { path: '/table', component: TablePage }
+        { path: '/table', component: TablePage },
+        { path: '/', component: AboutPage }
     ]
 });
