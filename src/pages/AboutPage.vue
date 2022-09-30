bf07f618571cd9c29eb0df602d637d70813e300a
Тисло Артём Максимович <tgeorigen@mail.ru>
2022-10-01 00:56:43 +0600

    2412412

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -4,8 +4,8 @@
         </h1>
         <input type="button" onclick="location.href='#/table';" style="flex: 1; font-size: 20px; height: 50px;" value="БАЗА ЗНАНИЙ"/>
     </div>
-    <h2 style="padding: 25px; margin: 0; padding-bottom: 0; font-size: 38px;">Описание:</h2>
-    <h3 style="padding: 25px; padding-top: 0;">Описания пока нет, но оно точно будет !!!</h3>
+    <!-- <h2 style="padding: 25px; margin: 0; padding-bottom: 0; font-size: 38px;">Описание:</h2>
+    <h3 style="padding: 25px; padding-top: 0;">Описания пока нет, но оно точно будет !!!</h3> -->
 </template>
   
 <script>
